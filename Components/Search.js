b83ee3cf381b1df9c8cbad8d7c2aec912e67debb
sofiane <monzinou1@gmail.com>
2020-04-19 06:27:51 +0100

    add loading and image for movies

--- Components/Search.js
+++ Components/Search.js
@@ -1,23 +1,33 @@
 
 import React from 'react'
-import {StyleSheet,View, TextInput, Button ,FlatList,Text} from 'react-native'
+import {ActivityIndicator,StyleSheet,View, TextInput, Button ,FlatList,Text} from 'react-native'
 //import films from  '../Helpers/filmsData'
 import FilmItem from './FilmItem'
 import {getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
+
 class Search extends React.Component{
 
   constructor(props)
   {
     super(props)
     this.searchedText=""
-    this.state={films:[]}
+    this.state={films:[],isLoading:false}
   }
-
+_displayLoading(){
+  if(this.state.isLoading){
+    return(
+      <View style={styles.loading_container}>
+<ActivityIndicator size='large'/>
+      </View>
+    )
+  }
+}
 _loadFilms(){
   console.log(this.searchedText)
   if(this.searchedText.length>0){
+    this.setState({ isLoading: true })
   getFilmsFromApiWithSearchedText(this.searchedText).then(data=>{
-    this.setState({films: data.results})
+    this.setState({films: data.results,   isLoading:false})
 
   })
   .catch((error) => console.error(error))
@@ -27,9 +37,7 @@ _searchedTextInputChanged(text){
   this.searchedText=text
 }
 
-
   render() {
-
      return (
        <View style={styles.main_container}>
          <TextInput style={styles.textinput} placeholder='Titre du film'
@@ -41,6 +49,7 @@ _searchedTextInputChanged(text){
          keyExtractor={(item) => item.id.toString()}
          renderItem={({item}) => <FilmItem film={item}/>}
          />
+         {this._displayLoading()}
        </View>
      );
    }
@@ -57,7 +66,15 @@ const styles =StyleSheet.create( {
     borderColor: '#000000',
     borderWidth: 1,
     paddingLeft: 5
-
+  },
+  loading_container:{
+    position: 'absolute',
+   left: 0,
+   right: 0,
+   top: 100,
+   bottom: 0,
+   alignItems: 'center',
+   justifyContent: 'center'
   }
 })
 export default Search
