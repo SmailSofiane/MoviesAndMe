75dc9afca4b9b4f0ac16558bec3e9fa873ee9a3e
sofiane <monzinou1@gmail.com>
2020-04-22 12:14:58 +0100

    FilmDetaill navigation complete

--- Components/FilmDetail.js
+++ Components/FilmDetail.js
@@ -1,12 +1,55 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text ,ActivityIndicator,ScrollView,Image} from 'react-native'
+import {getFilmDetailFromApi,getImageFromApi} from '../API/TMDBApi'
 
 class FilmDetail extends React.Component {
-  render() {
+constructor(props)
+{
+  super(props)
+  this.state={
+    film:undefined,
+    isLoading:true
+  }
+}
+componentDidMount() {
+  getFilmDetailFromApi(this.props.route.params.idFilm)
+  .then(data=>{
+    this.setState({film:data,
+    isLoading:false})
+  })
+ }
 
+  _displayLoading()
+  {
+    if(this.state.isLoading)
+    {
+      return(
+        <View style={styles.loading_container}>
+         <ActivityIndicator size='large' />
+       </View>
+      )
+    }
+  }
+  _displayFilm(){
+    if(this.state.film!=undefined){
+      return(
+        <ScrollView style={styles.scrollview_container}>
+        <Image   style={styles.image} source={{uri: getImageFromApi(this.state.film.poster_path)}}></Image>
+        <Text>{this.state.film.title}</Text>
+        <Text>{this.state.film.overview}</Text>
+        <Text>Sorti le:{this.state.film.release_date}</Text>
+        <Text>Note:{this.state.film.vote_average}/10</Text>
+        <Text>Budjet:{this.state.film.budget}</Text>
+        <Text>Genre(s):{this.state.film.genres}</Text>
+        </ScrollView>
+      )
+    }
+  }
+  render() {
     return (
       <View style={styles.main_container}>
-        <Text>Détail du film</Text>
+      {this._displayLoading()}
+      {this._displayFilm()}
       </View>
     )
   }
@@ -15,7 +58,22 @@ class FilmDetail extends React.Component {
 const styles = StyleSheet.create({
   main_container: {
     flex: 1,
-  }
+  },loading_container: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }, scrollview_container: {
+    flex: 1
+  },  image: {
+      width: 410,
+      height: 120,
+      margin:1,
+      backgroundColor: 'gray'
+    }
 })
 
 export default FilmDetail
