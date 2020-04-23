fda94716f111a61b391dc51d02ef5dd9e492fc75
sofiane <monzinou1@gmail.com>
2020-04-23 10:10:21 +0100

    create a Store

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Navaigation from  './Navigation/Navigation'
+import {provider, Provider} from 'react-redux'
+import Store from './Store/configureStore'
 
 export default class App extends React.Component {
     render(){
   return (
+    <Provider store={Store}>
    <Navaigation/>
+   </Provider>
   );
 
 }
---
+++ Store/configureStore.js
@@ -0,0 +1,6 @@
+import {createStore} from 'redux'
+import toggleFavorite from './Reduceres/favoriteReducer'
+
+
+
+export default createStore(toggleFavorite) 
